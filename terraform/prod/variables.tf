1f00ca6dfdf44feec9a41064bea2711f4cb59ecb
mauro1012 <mauricio.correa369@gmail.com>
2026-01-03 00:26:28 -0500

    Add QA environment infrastructure and CI/CD pipeline

---
+++ terraform/prod/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
---
+++ terraform/qa/terraform.tfvars
@@ -0,0 +1,3 @@
+environment   = "qa"
+vpc_cidr      = "10.0.0.0/16"
+instance_type = "t3.micro"
