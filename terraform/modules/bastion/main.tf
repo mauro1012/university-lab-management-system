2477fe400915325b134bc10b6605c6ff291aeba2
mauro1012 <mauricio.correa369@gmail.com>
2026-01-04 18:47:04 -0500

    changes

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = "t3.micro"
   subnet_id     = var.public_subnet
-  key_name      = aws.key_name
+  key_name      = var.key_name
 
   tags = {
     Name = "qa-bastion"
