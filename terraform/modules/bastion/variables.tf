a8c35f36a5ce1ec318d67bd887fdca4c8c736674
mauro1012 <mauricio.correa369@gmail.com>
2026-01-06 14:38:05 -0500

    chec

--- terraform/modules/bastion/variables.tf
+++ terraform/modules/bastion/variables.tf
@@ -1,15 +1,24 @@
-variable "public_subnet" {
-  type = string
-}
-
-variable "key_name" {
-  type = string
+variable "environment" {
+  type        = string
+  description = "Environment name (qa, prod)"
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
+  description = "Instance type for bastion"
 }
 
-variable "environment" {
-  type = string
+variable "ami_id" {
+  type        = string
+  description = "AMI for bastion host"
+}
+
+variable "key_name" {
+  type        = string
+  description = "SSH key pair name"
+}
+
+variable "public_subnet" {
+  type        = string
+  description = "Public subnet ID"
 }
