d1b3e14e604f3e09b8f2e6082950c38c6734f32b
mauro1012 <mauricio.correa369@gmail.com>
2026-01-12 00:33:57 -0500

    changes class alb

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -24,6 +24,18 @@ resource "aws_lb" "this" {
   security_groups    = [aws_security_group.alb.id]
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+
 resource "aws_lb_target_group" "this" {
   name     = "${var.env}-${var.service_name}-tg"
   port     = 8080
