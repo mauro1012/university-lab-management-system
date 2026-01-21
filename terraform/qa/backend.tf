2f02f03ec2ea46f72629815894c3c88479392dfd
mauro1012 <mauricio.correa369@gmail.com>
2026-01-21 12:18:15 -0500

    fix: nombre de bucket s3 en minúsculas para cumplir reglas de AWS

--- terraform/qa/backend.tf
+++ terraform/qa/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "qa-terraform-state-21012023V2"
+    bucket         = "qa-terraform-state-21012023v2"
     key            = "qa/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks-qa"
