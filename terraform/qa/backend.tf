f1e89b860ece4bf70de972a0a94862639fac494f
mauro1012 <mauricio.correa369@gmail.com>
2026-01-21 13:55:09 -0500

    s3 new

--- terraform/qa/backend.tf
+++ terraform/qa/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "qa-terraform-state-21012023v2"
+    bucket         = "qa-terraform-state-21012023v3"
     key            = "qa/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks-qa"
